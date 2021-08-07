7ef57ec6943cafac5bcbd6146f28eb1ce778178a
manar-ali <69252463+manar-ali@users.noreply.github.com>
2021-08-07 21:35:14 +0200

    school system

---
+++ SchoolManagementSystem/SchoolManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,156 @@
+﻿using DatabaseAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SchoolMgtSysDbEntities db = new SchoolMgtSysDbEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginUser(string email,string password)
+        {
+            try
+            {
+                if(email !=null && password != null)
+                {
+                    var finduser = db.UserTables.Where(u => u.EmailAddress == email && u.Password == password).ToList();
+                    if(finduser.Count() == 1)
+                    {
+                        Session["UserName"] = finduser[0].UserName;
+                        Session["Password"] = finduser[0].Password;
+                        Session["UserTypeID"] = finduser[0].UserTypeID;
+                        Session["Address"] = finduser[0].Address;
+                        Session["FullName"] = finduser[0].FullName;
+                        Session["ContactNo"] = finduser[0].ContactNo;
+                        Session["EmailAddress"] = finduser[0].EmailAddress;
+                        Session["UserID"] = finduser[0].UserID;
+                        var userid = finduser[0].UserID;
+
+                        var studentphoto = db.StudentTables.Where(s =>s.UserID == userid).FirstOrDefault();
+                        if(studentphoto != null)
+                        {
+                            Session["Photo"] = studentphoto.Photo;
+                        }
+                        else
+                        {
+                            var employee = db.StaffTables.Where(e => e.UserID == userid).FirstOrDefault();
+                            Session["Photo"] = employee.Photo;
+                        }
+
+                        string url = string.Empty;
+                        if(finduser[0].UserTypeID == 2)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 3)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 1)
+                        {
+                            url = "About";
+                        }
+                        else
+                        {
+                            url = "About";
+                        }
+                        return RedirectToAction(url);
+                    }
+                    else
+                    {
+                        Session["UserName"] = string.Empty;
+                        Session["Password"] = string.Empty;
+                        Session["UserTypeID"] = string.Empty;
+                        Session["Address"] = string.Empty;
+                        Session["FullName"] = string.Empty;
+                        Session["ContactNo"] = string.Empty;
+                        Session["EmailAddress"] = string.Empty;
+                        Session["UserID"] = string.Empty;
+                        ViewBag.message = "user Name or Passwerd isn't correct";
+                    }
+                }
+                else
+                {
+                    Session["UserName"] = string.Empty;
+                    Session["Password"] = string.Empty;
+                    Session["UserTypeID"] = string.Empty;
+                    Session["Address"] = string.Empty;
+                    Session["FullName"] = string.Empty;
+                    Session["ContactNo"] = string.Empty;
+                    Session["EmailAddress"] = string.Empty;
+                    Session["UserID"] = string.Empty;
+                    ViewBag.message = "Some unexpected issue is occure Please Try Agin";
+                }
+            }
+            catch(Exception ex)
+            {
+                Session["UserName"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["UserTypeID"] = string.Empty;
+                Session["Address"] = string.Empty;
+                Session["FullName"] = string.Empty;
+                Session["ContactNo"] = string.Empty;
+                Session["EmailAddress"] = string.Empty;
+                Session["UserID"] = string.Empty;
+                ViewBag.message = "Some unexpected issue is occure Please Try Agin";
+            }
+            return View("Login");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome to School";
+
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePasswordU(string oldpassword, string newpassword, string confirmpassword)
+        {
+            if(newpassword != confirmpassword)
+            {
+                ViewBag.Message = "Not Matched !!";
+                return View("ChangePassword");
+            }
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var getuser = db.UserTables.Find(userid);
+            if(getuser.Password == oldpassword.Trim())
+            {
+                getuser.Password = newpassword.Trim();
+            }
+            else
+            {
+                ViewBag.Message = "Old Password Is Incorrect !!";
+                return View("ChangePassword");
+            }
+            db.Entry(getuser).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Logout");
+        }
+
+        public ActionResult Logout()
+        {
+            Session["UserName"] = string.Empty;
+            Session["Password"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            Session["Address"] = string.Empty;
+            Session["FullName"] = string.Empty;
+            Session["ContactNo"] = string.Empty;
+            Session["EmailAddress"] = string.Empty;
+            Session["UserID"] = string.Empty;
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ SchoolManagementSystem/SchoolManagementSystem/Controllers/StudentTablesController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAccess;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class StudentTablesController : Controller
+    {
+        private SchoolMgtSysDbEntities db = new SchoolMgtSysDbEntities();
+       
+        // GET: StudentTables
+        public ActionResult Index()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var studentTables = db.StudentTables.Include(s => s.ProgrameTable).Include(s => s.SessionTable).Include(s => s.UserTable).Include(s => s.ClassTable);
+            return View(studentTables.ToList());
+        }
+
+        // GET: StudentTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Create
+        public ActionResult Create()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            ViewBag.ProgrameID = new SelectList(db.ProgrameTables, "ProgrameID", "Name");
+            ViewBag.SessionID = new SelectList(db.SessionTables, "SessionID", "Name");
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName");
+            ViewBag.ClassID = new SelectList(db.ClassTables, "ClassID", "Name");
+            return View();
+        }
+
+        // POST: StudentTables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StudentTable studentTable)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            studentTable.UserID = userid;
+            studentTable.Photo = "/Content/StudentPhoto/default.png";
+
+            if (ModelState.IsValid)
+            {
+                var user = new UserTable();
+                user.Address = studentTable.Address;
+                user.ContactNo = studentTable.ContactNo;
+                user.EmailAddress = studentTable.EmailAddress;
+                user.FullName = studentTable.Name;
+                user.UserName = studentTable.Name;
+                user.UserTypeID = 3;
+                user.Password = "123456";
+                db.UserTables.Add(user);
+                studentTable.UserID = user.UserID;
+
+                db.StudentTables.Add(studentTable);
+                db.SaveChanges();
+                if (studentTable.PhotoFile != null)
+                {
+                    var folder = "/Content/StudentPhoto";
+                    var file = string.Format("{0}.png", studentTable.StudentID);
+                    var response = FileHelper.UploadFile.UploadPhoto(studentTable.PhotoFile, folder, file);
+                    if (response)
+                    {
+                        var pic = string.Format("{0}/{1}", folder, file);
+                        studentTable.Photo = pic;
+                        db.Entry(studentTable).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProgrameID = new SelectList(db.ProgrameTables, "ProgrameID", "Name", studentTable.ProgrameID);
+            ViewBag.SessionID = new SelectList(db.SessionTables, "SessionID", "Name", studentTable.SessionID);
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", studentTable.UserID);
+            ViewBag.ClassID = new SelectList(db.ClassTables, "ClassID", "Name", studentTable.ClassID);
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProgrameID = new SelectList(db.ProgrameTables, "ProgrameID", "Name", studentTable.ProgrameID);
+            ViewBag.SessionID = new SelectList(db.SessionTables, "SessionID", "Name", studentTable.SessionID);
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", studentTable.UserID);
+            ViewBag.ClassID = new SelectList(db.ClassTables, "ClassID", "Name", studentTable.ClassID);
+            return View(studentTable);
+        }
+
+        // POST: StudentTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(StudentTable studentTable)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            studentTable.UserID = userid;
+            if (ModelState.IsValid)
+            {
+                var folder = "/Content/StudentPhoto";
+                var file = string.Format("{0}.png", studentTable.StudentID);
+                var response = FileHelper.UploadFile.UploadPhoto(studentTable.PhotoFile, folder, file);
+                if (response)
+                {
+                    var pic = string.Format("{0}/{1}", folder, file);
+                    studentTable.Photo = pic;
+                }
+
+                db.Entry(studentTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProgrameID = new SelectList(db.ProgrameTables, "ProgrameID", "Name", studentTable.ProgrameID);
+            ViewBag.SessionID = new SelectList(db.SessionTables, "SessionID", "Name", studentTable.SessionID);
+            ViewBag.UserID = new SelectList(db.UserTables, "UserID", "FullName", studentTable.UserID);
+            ViewBag.ClassID = new SelectList(db.ClassTables, "ClassID", "Name", studentTable.ClassID);
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // POST: StudentTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            db.StudentTables.Remove(studentTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
